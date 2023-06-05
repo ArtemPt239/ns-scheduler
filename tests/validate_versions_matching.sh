8383fecff72a925f9fe8773c434a8809d9766113
ArtemPt239 <artem99petrov@gmail.com>
2023-06-05 19:40:04 +0400

    more version validation

--- tests/validate_versions_matching.sh
+++ tests/validate_versions_matching.sh
@@ -3,10 +3,15 @@
 
 git_latest_tag=$(git describe --tags --abbrev=0)
 helm_chart_appversion=$(cat helm/chart/Chart.yaml | grep appVersion | awk '{print "v" $2}')
+helm_chart_version=$(cat helm/chart/Chart.yaml | grep version | awk '{print $2}')
 
-if [ "$git_latest_tag" != "$helm_chart_appversion" ]; then
-  echo "The appVersion in the helm chart does not match the latest git tag"
+if [ "$git_latest_tag" != "$helm_chart_appversion" ] \ 
+  || [ "$git_latest_tag" != "$helm_chart_version" ] \
+  || [ "$helm_chart_appversion" != "$helm_chart_version" ]
+then
+  echo "Version mismatch detected!"
   echo "Latest git tag: $git_latest_tag"
   echo "appVersion in helm chart: $helm_chart_appversion"
+  echo "helm chart version: $helm_chart_version"
   exit 1
 fi
