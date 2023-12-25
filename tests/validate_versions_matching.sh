c22012370c5600b0c5b6190b958c88041054c422
ArtemPt239 <artem99petrov@gmail.com>
2023-12-26 01:59:33 +0400

    hotfix for github actions correct tags

--- tests/validate_versions_matching.sh
+++ tests/validate_versions_matching.sh
@@ -2,8 +2,8 @@
 # It is used to ensure that the appVersion is updated when a new release is created
 
 git_latest_tag=$(git describe --tags --abbrev=0)
-helm_chart_appversion=$(cat helm/chart/Chart.yaml | grep appVersion | awk '{print "v" $2}')
-helm_chart_version=$(cat helm/chart/Chart.yaml | grep version | awk '{print "v" $2}')
+helm_chart_appversion=$(cat helm/chart/Chart.yaml | grep appVersion | awk '{print $2}')
+helm_chart_version=$(cat helm/chart/Chart.yaml | grep version | awk '{print $2}')
 
 if [ "$git_latest_tag" != "$helm_chart_appversion" ] || \
   [ "$git_latest_tag" != "$helm_chart_version" ] || \
